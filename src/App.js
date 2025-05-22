116b84c82e01b1295fd1d86ca2fa09bed4bfd0ae
chase <yicha7@gmail.com>
2025-05-22 13:14:36 +0900

    sdf

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import Member from './Member';
+import { useState } from 'react';
 
 function App() {
-  
+  const [data,setData] = useState([]);
   axios.get('/admin/api/p_list.php')
   .then(res=>{
-    console.log(res.data);
+    setData(res.data);
   })
 
+  if(!data.length) return;
 
   return (
     <div className="App">
+      <img src={`http://chase.dothome.co.kr/admin/product/upload/${data[0].p_thumb}`} />
 
       
     </div>
